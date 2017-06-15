58100298976d365d2e7d63cc3e8b230e93513fd5
samzong <luchuanjia@msn.com>
2017-06-15 14:51:38 +0800

    增加飞船模块
    添加ship.py中Ship类，并在主程序中引入，在窗口显示飞船，ship.jpg放在images下，注意这里使用pygame.image.load()引入时需要用绝对路径.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,15 +3,23 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
-    # 初始化游戏并创建一个屏幕对象
+    # 初始化pygame、设置和屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    # screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
     pygame.display.set_caption("Alien Incasion")
 
     # 设置背景颜色
-    bg_color = (233,233,233)
+    # bg_color = (233,233,233)
+
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     # 开始游戏的主循环
     while True:
@@ -22,7 +30,9 @@ def run_game():
                 sys.exit()
 
         # 每次循环时都重绘屏幕
-        screen.fill(bg_color)
+        # screen.fail(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
