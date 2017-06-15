cd5311b65cc662cc9dc3391f2e6e3ad758e33f9d
samzong <luchuanjia@msn.com>
2017-06-15 16:04:05 +0800

    测验题目-练习
    将飞船初始位置改为屏幕居中，并可以通过上下左右移动.

--- game_functions.py
+++ game_functions.py
@@ -10,6 +10,10 @@ def check_events(ship):
             ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            ship.moving_down = True
 
     def check_keyup_events(event, ship):
         """响应松开"""
@@ -17,6 +21,10 @@ def check_events(ship):
             ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            ship.moving_down = False
 
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
--- ship.py
+++ ship.py
@@ -16,16 +16,19 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        # 将每艘新飞船放在屏幕底部中央
+        # 将每艘新飞船放在屏幕中央, 使用 centerx & centery确认飞船坐标
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.centery = self.screen_rect.centery
 
         # 在飞船的属性center中存储小数值
-        self.center = float(self.rect.centerx)
+        self.center_x = float(self.rect.centerx)
+        self.center_y = float(self.rect.centery)
 
         # 移动标志
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """根据移动标志调整飞船的位置"""
@@ -33,14 +36,26 @@ class Ship():
         # if self.moving_right:
         if self.moving_right and self.rect.right < self.screen_rect.right:
             # self.rect.centerx += 1
-            self.center += self.ai_settings.ship_speed_factor
+            self.center_x += self.ai_settings.ship_speed_factor
         # if self.moving_left:
         if self.moving_left and self.rect.left > 0:
             # self.rect.centerx -= 1
-            self.center -= self.ai_settings.ship_speed_factor
+            self.center_x -= self.ai_settings.ship_speed_factor
 
         # 根据self.center更新rect对象
-        self.rect.centerx = self.center
+        self.rect.centerx = self.center_x
+
+        # if self.moving_up:
+        #     self.rect.centery -= 1
+        if self.moving_up and self.rect.right < self.screen_rect.right:
+            self.center_y -= self.ai_settings.ship_speed_factor
+
+        # if self.moving_down:
+        #     self.rect.centery += 1
+        if self.moving_down and self.rect.right > 0:
+            self.center_y += self.ai_settings.ship_speed_factor
+
+        self.rect.centery = self.center_y
 
     def blitme(self):
         """在指定位置绘制飞船"""
